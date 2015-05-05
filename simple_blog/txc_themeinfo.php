425846ac741b4b25e5589719dec659089a22072d
mambax7 <mambax7@gmail.com>
2015-05-05 09:29:17 -0400

    Original files

---
+++ simple_blog/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "simple-blog";
+	$txc_themeinfo["author"] = "phppp";
+	$txc_themeinfo["author_url"] = "http://www.xoopsforge.com";
+	$txc_themeinfo["detail_url"] = "http://xoops.org.cn/modules/newbb/viewtopic.php?topic_id=400&forum=4&post_id=1735#forumpost1735";
+	$txc_themeinfo["download_url"] = "http://xoops.org.cn/modules/wfdownloads/visit.php?cid=6&lid=156";
+?>
